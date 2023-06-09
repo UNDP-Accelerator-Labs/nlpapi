b40d108adbc496c17cd25e65e1c35297790433fd
Josua Krause <josua.krause@gmail.com>
2023-06-09 14:25:04 -0400

    docker functions

---
+++ sh/dockerpush.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../" &> /dev/null
+
+IMAGE_NAME="smartsearch-$(make -s name)"
+DOCKER_LOGIN_SERVER="acclabdocker.azurecr.io"
+DOCKER_IMAGE_URL="${DOCKER_LOGIN_SERVER}/${IMAGE_NAME}"
+
+docker tag "${IMAGE_NAME}" "${DOCKER_IMAGE_URL}"
+docker push "${DOCKER_IMAGE_URL}"
+docker rmi "${DOCKER_IMAGE_URL}"
