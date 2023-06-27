54e4fe3eff9048c9ed49c0e5da934a477a312822
Josua Krause <josua.krause@gmail.com>
2023-06-27 14:17:17 -0400

    show errors

--- app/system/location/pipeline.py
+++ app/system/location/pipeline.py
@@ -60,7 +60,8 @@ def extract_locations(
     for entity in entities:
         query, start, stop = entity
         if query != input_text[start:stop]:
-            raise ValueError(f"oh no: {query} {start} {stop}")
+            raise ValueError(
+                f"oh no: '{query}' {start} {stop} '{input_text[start:stop]}'")
         info = entity_map.get(query, None)
         if info is None:
             loc, status = get_resp(query)
