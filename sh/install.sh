8d6afff0b1a0afb23441faea3c78203789be6f89
Josua Krause <josua.krause@gmail.com>
2023-10-04 17:53:21 -0400

    require python 3.11

--- sh/install.sh
+++ sh/install.sh
@@ -13,8 +13,8 @@ fi
 MAJOR=$(${PYTHON} -c 'import sys; print(sys.version_info.major)')
 MINOR=$(${PYTHON} -c 'import sys; print(sys.version_info.minor)')
 echo "${PYTHON} v${MAJOR}.${MINOR}"
-if [ ${MAJOR} -eq 3 ] && [ ${MINOR} -lt 10 ] || [ ${MAJOR} -lt 3 ]; then
-    echo "${PYTHON} version must at least be 3.10" >&2
+if [ ${MAJOR} -eq 3 ] && [ ${MINOR} -lt 11 ] || [ ${MAJOR} -lt 3 ]; then
+    echo "${PYTHON} version must at least be 3.11" >&2
     exit 1
 fi
 
