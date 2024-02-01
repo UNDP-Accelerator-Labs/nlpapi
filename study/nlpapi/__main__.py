9c01f97009d96e9056e0014dd48b19578a4696b7
Josua Krause <josua.krause@gmail.com>
2024-02-01 12:12:40 -0500

    use latest smind functions

--- study/nlpapi/__main__.py
+++ study/nlpapi/__main__.py
@@ -1,57 +1,31 @@
 import json
 import time
 
-from scattermind.system.client.loader import load_client_pool
-from scattermind.system.config.config import Config
-from scattermind.system.logger.loader import load_event_listener
-from scattermind.system.logger.log import EventStream
-from scattermind.system.payload.loader import load_store
-from scattermind.system.payload.values import TaskValueContainer
-from scattermind.system.queue.loader import load_queue_pool
-from scattermind.system.queue.strategy.loader import (
-    load_node_strategy,
-    load_queue_strategy,
-)
-from scattermind.system.readonly.loader import load_readonly_access
-from scattermind.system.torch_util import str_to_tensor
+from scattermind.api.api import ScattermindAPI
+from scattermind.api.loader import load_api
 
 
-def load_config() -> Config:
+def load_config() -> ScattermindAPI:
     with open("config.json", "rb") as fin:
         config_obj = json.load(fin)
-    config = Config()
-    logger = EventStream()
-    logger_obj = config_obj["logger"]
-    for listener_def in logger_obj["listeners"]:
-        logger.add_listener(
-            load_event_listener(listener_def, logger_obj["disable_events"]))
-    config.set_logger(logger)
-    config.set_data_store(load_store(config_obj["data_store"]))
-    config.set_queue_pool(load_queue_pool(config_obj["queue_pool"]))
-    config.set_client_pool(load_client_pool(config_obj["client_pool"]))
-    strategy_obj = config_obj["strategy"]
-    config.set_node_strategy(load_node_strategy(strategy_obj["node"]))
-    config.set_queue_strategy(load_queue_strategy(strategy_obj["queue"]))
-    config.set_readonly_access(
-        load_readonly_access(config_obj["readonly_access"]))
-    return config
+    return load_api(config_obj)
 
 
 def run() -> None:
-    config = load_config()
+    smind = load_config()
     with open("graph.json", "rb") as fin:
         graph_def_obj = json.load(fin)
-    config.load_graph(graph_def_obj)
+    smind.load_graph(graph_def_obj)
     real_start = time.monotonic()
-    task_id_0 = config.enqueue(TaskValueContainer({
-        "text": str_to_tensor("abc").clone().detach(),
-    }))
+    task_id_0 = smind.enqueue_task({
+        "text": "abc",
+    })
     print(f"enqueued {task_id_0}")
-    task_id_1 = config.enqueue(TaskValueContainer({
-        "text": str_to_tensor("hallo").clone().detach(),
-    }))
+    task_id_1 = smind.enqueue_task({
+        "text": "hallo",
+    })
     print(f"enqueued {task_id_1}")
-    for tid, resp in config.wait_for([task_id_0, task_id_1], timeout=60.0):
+    for tid, resp in smind.wait_for([task_id_0, task_id_1], timeout=60.0):
         status = resp["status"]
         duration = resp["duration"]
         real_time = time.monotonic() - real_start
