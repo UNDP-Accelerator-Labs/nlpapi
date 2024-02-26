29312dca006d32c860aa5a55455e81d9416cae60
Josua Krause <josua.krause@gmail.com>
2024-02-25 23:27:30 -0500

    use DEVICE as variable

--- study/run.sh
+++ study/run.sh
@@ -12,15 +12,15 @@ start_redis rmain 6381 &
 start_redis rdata 6383 &
 start_redis rcache 6382 &
 
-# if no gemma do GPU=1
-GPU="${GPU:-}"
+# if no gemma do DEVICE=auto
+DEVICE="${DEVICE:-cpu}"
 if command -v nvidia-smi &> /dev/null; then
-    GPU=1
+    DEVICE=auto
 fi
 
 cd ..
-if [ -z "${GPU}" ]; then
-    python -m scattermind --config study/config.json --device cpu worker --graph study/graphs/
-else
+if [ "${DEVICE}" = "auto" ]; then
     python -m scattermind --config study/config.json worker --graph study/graphs/
+else
+    python -m scattermind --config study/config.json --device "${DEVICE}" worker --graph study/graphs/
 fi
