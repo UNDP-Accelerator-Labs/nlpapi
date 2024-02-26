33fd80470f52e4ee65c1d31280c92d535550c76d
Josua Krause <josua.krause@gmail.com>
2024-02-25 23:08:49 -0500

    more model configs

---
+++ study/bench.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -ex
+
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../" &> /dev/null
+
+GPU="${GPU:-}"
+if command -v nvidia-smi &> /dev/null; then
+    GPU=1
+fi
+
+if [ ! -z "${GPU}" ]; then
+    nvidia-smi
+fi
+
+python -m nlpapi --config study/config.json --graph study/graphs/graph_gemma.json --input 'tell me about the tallest mountain in the world' --output -
+
+python -m nlpapi --config study/config.json --graph study/graphs/graph_gemma.json --input @study/prompts/extract/test0.txt --output -
+
+python -m nlpapi --config study/config.json --graph study/graphs/graph_gemma.json --input @study/prompts/extract/ --output -
+
+if [ ! -z "${GPU}" ]; then
+    python -m nlpapi --config study/config.json --graph study/graphs/graph_gemma7b.json --input @study/prompts/extract/test0.txt --output -
+fi
--- study/run.sh
+++ study/run.sh
@@ -12,7 +12,15 @@ start_redis rmain 6381 &
 start_redis rdata 6383 &
 start_redis rcache 6382 &
 
+# if no gemma do GPU=1
+GPU="${GPU:-}"
+if command -v nvidia-smi &> /dev/null; then
+    GPU=1
+fi
+
 cd ..
-# use if no gemma
-# python -m scattermind --config study/config.json worker --graph study/graphs/
-python -m scattermind --config study/config.json --device cpu worker --graph study/graphs/
+if [ -z "${GPU}" ]; then
+    python -m scattermind --config study/config.json --device cpu worker --graph study/graphs/
+else
+    python -m scattermind --config study/config.json worker --graph study/graphs/
+fi
