847f96681e229a6627c0b08c6ed98440b6e27a9e
Josua Krause <josua.krause@gmail.com>
2024-03-01 15:36:37 -0500

    gracefully stop

--- sh/compose.sh
+++ sh/compose.sh
@@ -4,4 +4,4 @@ set -e
 
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../" &> /dev/null
 
-docker compose -f deploy/docker-compose.yml --env-file deploy/default.env up
+docker compose -f deploy/docker-compose.yml --env-file deploy/default.env up || echo "done $?"
