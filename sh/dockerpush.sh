7ebd3583c10e776605bf9fc02650e1a207170d76
Josua Krause <josua.krause@gmail.com>
2024-03-01 15:52:09 -0500

    create docker compose on push

--- sh/dockerpush.sh
+++ sh/dockerpush.sh
@@ -5,6 +5,7 @@ cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../" &> /dev/null
 IMAGE_TAG="${IMAGE_TAG:-$(make -s name)}"
 IMAGE_NAME="nlpapi:${IMAGE_TAG}"
 DOCKER_LOGIN_SERVER="acclabdocker.azurecr.io"
+PYTHON="${PYTHON:-python3}"
 
 dpush() {
     IMAGE="${IMAGE_NAME}-$1"
@@ -21,3 +22,29 @@ dpush "api"
 dpush "rmain"
 dpush "rdata"
 dpush "rcache"
+
+! read -r -d '' PY_COMPOSE <<'EOF'
+import os
+import sys
+
+dcompose = sys.argv[1]
+denv = sys.argv[2]
+dout = sys.argv[3]
+substitute = {}
+with open(denv, "r", encoding="utf-8") as fin:
+    for line in fin:
+        line = line.rstrip().split("#", 1)[0].strip()
+        if not line:
+            continue
+        variable, value = line.split("=", 1)
+        substitute[f"${variable}".strip()] = value.strip()
+with open(dcompose, "r", encoding="utf-8") as din:
+    content = din.read()
+for variable, value in sorted(
+        substitute.items(), key=lambda e: len(e[0]), reverse=True):
+    content = content.replace(variable, value)
+with open(dout, "w", encoding="utf-8") as fout:
+    fout.write(content)
+EOF
+
+${PYTHON} -c "${PY_COMPOSE}" "deploy/docker-compose.yml" "deploy/default.env" "docker-compose.yml"
