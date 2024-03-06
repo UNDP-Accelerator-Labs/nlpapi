57cae436b4997c0e8e5e1dea073ed0581861a8cb
Josua Krause <josua.krause@gmail.com>
2024-03-06 10:10:18 -0500

    create docker compose on build

--- sh/build.sh
+++ sh/build.sh
@@ -5,6 +5,10 @@ set -e
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../" &> /dev/null
 
 mkdir -p buildtmp
+
+DOCKER_LOGIN_SERVER="acclabdocker.azurecr.io"
+PYTHON="${PYTHON:-python3}"
+
 DOCKER_CONFIG=docker.config.json
 LOCAL_CONFIG=config.json
 NO_CONFIG=noconfig.json
@@ -86,6 +90,8 @@ if [ ! -z "${DEV}" ]; then
         -t "${IMAGE_NAME}-rcache" \
         -f deploy/redis.Dockerfile \
         .
+
+    DOCKER_COMPOSE_OUT="docker-compose.dev.yml"
 else
     docker buildx build \
         --platform linux/amd64 \
@@ -128,6 +134,8 @@ else
         -t "${IMAGE_NAME}-rcache" \
         -f deploy/redis.Dockerfile \
         .
+
+    DOCKER_COMPOSE_OUT="docker-compose.yml"
 fi
 
 echo "# created by sh/build.sh" > deploy/default.env
@@ -143,3 +151,30 @@ echo "built ${IMAGE_NAME}-worker"
 echo "built ${IMAGE_NAME}-rmain"
 echo "built ${IMAGE_NAME}-rdata"
 echo "built ${IMAGE_NAME}-rcache"
+
+! read -r -d '' PY_COMPOSE <<'EOF'
+import os
+import sys
+
+prefix = sys.argv[1]
+dcompose = sys.argv[2]
+denv = sys.argv[3]
+dout = sys.argv[4]
+substitute = {}
+with open(denv, "r", encoding="utf-8") as fin:
+    for line in fin:
+        line = line.rstrip().split("#", 1)[0].strip()
+        if not line:
+            continue
+        variable, value = line.split("=", 1)
+        substitute[f"${variable}".strip()] = f"{prefix}/{value.strip()}"
+with open(dcompose, "r", encoding="utf-8") as din:
+    content = din.read()
+for variable, value in sorted(
+        substitute.items(), key=lambda e: len(e[0]), reverse=True):
+    content = content.replace(variable, value)
+with open(dout, "w", encoding="utf-8") as fout:
+    fout.write(content)
+EOF
+
+${PYTHON} -c "${PY_COMPOSE}" "${DOCKER_LOGIN_SERVER}" "deploy/docker-compose.yml" "deploy/default.env" "${DOCKER_COMPOSE_OUT}"
