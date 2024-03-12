94b395676233e8799d87266b1d1c756ac0578cb4
Josua Krause <josua.krause@gmail.com>
2024-03-11 23:46:01 -0400

    set -e

--- sh/git_check.sh
+++ sh/git_check.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../" &> /dev/null
 
 # see https://stackoverflow.com/a/2659808/20487202
--- sh/pyi.sh
+++ sh/pyi.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../" &> /dev/null
 
 PY_FILES=$(find stubs -type f -name '*.py')
