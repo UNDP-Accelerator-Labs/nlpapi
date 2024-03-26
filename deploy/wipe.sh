48f3017a57d2a4980fb24c25b2c0a8dab5d0d8a8
Josua Krause <josua.krause@gmail.com>
2024-03-26 18:02:19 -0400

    correct command

--- deploy/wipe.sh
+++ deploy/wipe.sh
@@ -3,20 +3,20 @@
 set -e
 
 remove() {
-  find "$1" -name . -o -prune -exec rm -rfv -- {} +
+  find "$1" ! -name "$1" -a -prune -exec rm -rfv -- {} +
 }
 
 echo "deleting /smind_cache"
-remove /smind_cache/
+remove /smind_cache
 echo "deleting /rbody"
-remove /rbody/
+remove /rbody
 echo "deleting /rcache"
-remove /rcache/
+remove /rcache
 echo "deleting /rdata"
-remove /rdata/
+remove /rdata
 echo "deleting /rmain"
-remove /rmain/
+remove /rmain
 echo "deleting /qdrant_data"
-remove /qdrant_data/
+remove /qdrant_data
 
 echo "done"
