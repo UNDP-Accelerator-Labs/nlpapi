96397ed8e6c952b7d029f41f4e7d3e3ac6548f82
Josua Krause <josua.krause@gmail.com>
2024-08-13 15:32:53 -0400

    started smind

--- app/system/smind/adder.py
+++ app/system/smind/adder.py
@@ -13,6 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""The semantic search document adder processing queue."""
 import uuid
 from collections.abc import Callable
 from typing import Literal, Protocol, TypedDict
